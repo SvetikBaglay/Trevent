ef661c1e2e1d1704c36a739aa93869a669c520aa
svetik <conbka@gmail.com>
2021-01-25 23:40:21 +0200

    jquery

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.trust__partners').slick({
+    arrows: true,
+    appendArrows: false,
+    prevArrow: $('.trust__shevron--left'),
+    nextArrow: $('.trust__shevron--right'),
+    slidesToShow: 4,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+});
